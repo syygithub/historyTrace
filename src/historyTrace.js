6525f3e5126ebb02f757437d74175155baff5e8a
songyy <songyy@zctt.com>
2020-05-14 10:11:40 +0800

    change historyTrace.js

--- src/historyTrace.js
+++ src/historyTrace.js
@@ -5,39 +5,39 @@ L.Marker.HistoryTrace = L.Marker.extend({
     startLatlng: undefined,
     endLatlng: undefined,
     _animateId: undefined,
+    polyline: undefined,
     initialize: function(latlngs, durations, options) {
         this.durations = durations ? durations : this.durations;
-        console.log(this);
-        console.log(this.durations);
         L.Marker.prototype.initialize.call(this, latlngs[0], options);
         this.startLatlng = L.latLng(latlngs[0]);
         this.endLatlng = L.latLng(latlngs[1]);
-        console.log(this.startLatlng);
-        console.log(this.endLatlng);
     },
     start: function() {
         console.log("start" + performance.now());
         this.moveToUntil = performance.now() + this.durations[0];
         this.fire('movestart');
         this._moveTo();
+        this.polyline = L.polyline([this.startLatlng, this.endLatlng], { color: 'green' }).addTo(this._map);
         return this;
     },
     _moveTo: function() {
         if (!this._map) return;
         var remaining = this.moveToUntil - performance.now();
-        console.log("remaining___" + remaining);
+        // console.log("remaining___" + remaining);
         if (remaining < 0) {
             this.setLatLng(L.latLng([this.endLatlng.lat, this.endLatlng.lng]));
             this.fire('moveend');
             return this;
         }
         var persent = (this.durations[0] - remaining) / this.durations[0];
-        console.log("persent" + persent);
+        // console.log("persent" + persent);
 
         var lat = this.startLatlng.lat + persent * (this.endLatlng.lat - this.startLatlng.lat);
         var lng = this.startLatlng.lng + persent * (this.endLatlng.lng - this.startLatlng.lng);
 
         this.setLatLng(L.latLng([lat, lng]));
+        this.polyline = L.polyline(L.latLng([lat, lng]), { color: 'red' }).addTo(this._map);
+
         this._animateId = L.Util.requestAnimFrame(this._moveTo, this)
 
     }
